68acaf34398c84e9fbdb13ef38596352e099c341
Jarred Taylor <jarrede20@gmail.com>
2022-06-12 20:31:47 -0400

    used the api to determine rather it will rain or not in the next 12 hours, if so, notify me to bring an umbrella

--- main.py
+++ main.py
@@ -8,7 +8,20 @@ weather_params = {
     "lat": 32.613003,
     "lon": -83.624199,
     "appid": api_key,
+    "exclude": "current,minutely,daily"
 }
 
 response = requests.get(OWM_Endpoint, params=weather_params)
-print(response.status_code)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+       will_rain = True  
+
+if will_rain:
+    print("Bring an umbrella!")
